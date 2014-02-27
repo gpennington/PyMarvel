17be10c700e909e81aa3a373e3af284b03c85f5f
Garrett Pennington <garrettp@gmail.com>
2014-02-27 14:00:23 -0600

    started test refactor to use less api calls

--- marvel/tests.py
+++ marvel/tests.py
@@ -15,44 +15,53 @@ class PyMarvelTestCase(unittest.TestCase):
 
     def setUp(self):
         self.m = Marvel(PUBLIC_KEY, PRIVATE_KEY)
+        self.character_dw = self.m.get_character(1009718)
+        self.character = self.character_dw.data.result
+        self.characters_dw = self.m.get_characters(orderBy="name,-modified", limit="10", offset="15")
+
+        #TODO: Need a comic with everything
+        #self.comic_dw = self.m.get_comic(531)
+        self.comic_dw = self.m.get_comic(17731)
+        self.comic = self.comic_dw.data.result
+        
+
 
     def tearDown(self):
         pass
 
+    #Character Tests
     def test_get_character(self):
-        cdw = self.m.get_character(1009718)
 
-        assert cdw.code == 200
-        assert cdw.status == 'Ok'
-        assert cdw.data.results[0].name == "Wolverine"
+        assert self.character_dw.code == 200
+        assert self.character_dw.status == 'Ok'
+        assert self.character.name == "Wolverine"
 
-        print "\nGet Character: \n"
-        print cdw.data.results[0].name
+        print "\nMarvel.get_character(): \n"
+        print self.character.name
 
-    def test_get_character_get_comics(self):
-        character = self.m.get_character(1009718).data.result
-        comic_dw = character.get_comics()
+    def test_character_get_comics(self):
+
+        comic_dw = self.character.get_comics()
 
         assert comic_dw.code == 200
         assert comic_dw.status == 'Ok'
         
-        print "\nWolverine comics: \n"
+        print "\Character.get_comics(): \n"
         for c in comic_dw.data.results:
             print "%s - %s" % (c.id, c.title)
 
-        comic_dw_params = character.get_comics(format="comic", formatType="comic", hasDigitalIssue=True, orderBy="title", limit=10, offset=30)
+        comic_dw_params = self.character.get_comics(format="comic", formatType="comic", hasDigitalIssue=True, orderBy="title", limit=10, offset=30)
 
         assert comic_dw_params.code == 200
         assert comic_dw_params.status == 'Ok'
         
-        print "\nWolverine comics with parameters: \n"
+        print "\nCharacter.get_comics(params): \n"
         for c in comic_dw_params.data.results:
             print "%s - %s" % (c.id, c.title)
 
+    def test_character_get_events(self):
 
-    def test_get_character_get_events(self):
-        character = self.m.get_character(1009718).data.result
-        events_dw = character.get_events()
+        events_dw = self.character.get_events()
 
         assert events_dw.code == 200
         assert events_dw.status == 'Ok'
@@ -61,7 +70,7 @@ class PyMarvelTestCase(unittest.TestCase):
         for e in events_dw.data.results:
             print "%s - %s" % (e.id, e.title)
 
-        events_dw_params = character.get_events(orderBy="startDate", limit=10)
+        events_dw_params = self.character.get_events(orderBy="startDate", limit=10)
 
         assert events_dw_params.code == 200
         assert events_dw_params.status == 'Ok'
@@ -71,76 +80,80 @@ class PyMarvelTestCase(unittest.TestCase):
             print "%s - %s" % (e.id, e.title)
 
     def test_get_characters(self):
-        cdw = self.m.get_characters(orderBy="name,-modified", limit="10", offset="15")
 
-        assert cdw.code == 200
-        assert cdw.status == 'Ok'
+        assert self.characters_dw.code == 200
+        assert self.characters_dw.status == 'Ok'
 
-        assert cdw.data.count > 0
-        assert cdw.data.offset == 15
-        assert cdw.data.limit == 10
-        assert len(cdw.data.results) > 0
+        assert self.characters_dw.data.count > 0
+        assert self.characters_dw.data.offset == 15
+        assert self.characters_dw.data.limit == 10
+        assert len(self.characters_dw.data.results) > 0
 
-        assert type(cdw) is CharacterDataWrapper
-        assert type(cdw.data) is CharacterDataContainer
-        assert type(cdw.data.results) is list
+        assert type(self.characters_dw) is CharacterDataWrapper
+        assert type(self.characters_dw.data) is CharacterDataContainer
+        assert type(self.characters_dw.data.results) is list
 
-        print "\nGet Characters: \n"
-        for c in cdw.data.results:
+        print "\nMarvel.get_characters():\n"
+        for c in self.characters_dw.data.results:
             print "%s - %s" % (c.id, c.name)
 
     def test_get_characters_next(self):
-        cdw = self.m.get_characters(orderBy="name,-modified", limit="20", offset="15")
-        new_cdw = cdw.next()
+        new_cdw = self.characters_dw.next()
 
         assert new_cdw.code == 200
 
         #poor test?
-        assert new_cdw.data.offset == cdw.data.offset + cdw.data.limit
+        assert new_cdw.data.offset == self.characters_dw.data.offset + self.characters_dw.data.limit
 
 
+
+
+
+    #Comic Tests
     def test_get_comic(self):
-        #Need a comic with everything
-        cdw = self.m.get_comic(531)
 
-        assert cdw.code == 200
-        assert cdw.status == 'Ok'
+        assert self.comic_dw.code == 200
+        assert self.comic_dw.status == 'Ok'
 
-        assert cdw.data.count > 0
-        assert cdw.data.offset == 0
-        assert len(cdw.data.results) > 0
+        assert self.comic_dw.data.count > 0
+        assert self.comic_dw.data.offset == 0
+        assert len(self.comic_dw.data.results) > 0
 
-        assert type(cdw) is ComicDataWrapper
-        assert type(cdw.data) is ComicDataContainer
-        assert type(cdw.data.results) is list
+        assert type(self.comic_dw) is ComicDataWrapper
+        assert type(self.comic_dw.data) is ComicDataContainer
+        assert type(self.comic_dw.data.results) is list
 
         #properties
         #textObjects
-        assert len(cdw.data.results[0].textObjects) > 0
-        assert isinstance(cdw.data.results[0].textObjects[0], TextObject)
+        #assert len(self.comic_dw.data.result.textObjects) > 0
+        #assert isinstance(self.comic_dw.data.result.textObjects[0], TextObject)
         #collections
-        assert isinstance(cdw.data.results[0].collections[0], ComicSummary)
+        assert isinstance(self.comic_dw.data.result.collections[0], ComicSummary)
         #prices/dates
-        assert isinstance(cdw.data.results[0].prices[0], ComicPrice)
-        assert isinstance(cdw.data.results[0].dates[0], ComicDate)
+        assert isinstance(self.comic_dw.data.result.prices[0], ComicPrice)
+        assert isinstance(self.comic_dw.data.result.dates[0], ComicDate)
         #images
-        assert isinstance(cdw.data.results[0].thumbnail, Image)
-        assert isinstance(cdw.data.results[0].images[0], Image)
+        assert isinstance(self.comic_dw.data.result.thumbnail, Image)
+        assert isinstance(self.comic_dw.data.result.images[0], Image)
         #lists
-        assert isinstance(cdw.data.results[0].creators, CreatorList)
-        assert isinstance(cdw.data.results[0].creators.items[0], CreatorSummary)
-        assert isinstance(cdw.data.results[0].characters, CharacterList)
-        assert isinstance(cdw.data.results[0].characters.items[0], CharacterSummary)
-        assert isinstance(cdw.data.results[0].stories, StoryList)
-        assert isinstance(cdw.data.results[0].stories.items[0], StorySummary)
+        assert isinstance(self.comic_dw.data.result.creators, CreatorList)
+        assert isinstance(self.comic_dw.data.result.creators.items[0], CreatorSummary)
+        assert isinstance(self.comic_dw.data.result.characters, CharacterList)
+        assert isinstance(self.comic_dw.data.result.characters.items[0], CharacterSummary)
+        assert isinstance(self.comic_dw.data.result.stories, StoryList)
+        assert isinstance(self.comic_dw.data.result.stories.items[0], StorySummary)
         #TODO: Need a test case with an Event
-        #assert isinstance(cdw.data.results[0].events, EventList)
-        #assert isinstance(cdw.data.results[0].events.items[0], EventSummary)
+        #assert isinstance(cdw.data.result.events, EventList)
+        #assert isinstance(cdw.data.result.events.items[0], EventSummary)
 
 
-    def test_get_comic_get_events(self):
-        comic = self.m.get_comic(17731).data.result
-        events_dw = comic.get_events()
+        print "\nMarvel.get_comic(): \n"
+        print self.comic.title
+
+
+    def test_comic_get_events(self):
+
+        events_dw = self.comic.get_events()
 
         assert events_dw.code == 200
         assert events_dw.status == 'Ok'
@@ -149,7 +162,7 @@ class PyMarvelTestCase(unittest.TestCase):
         for e in events_dw.data.results:
             print "%s - %s" % (e.id, e.title)
 
-        events_dw_params = comic.get_events(orderBy="startDate", limit=1)
+        events_dw_params = self.comic.get_events(orderBy="startDate", limit=1)
 
         assert events_dw_params.code == 200
         assert events_dw_params.status == 'Ok'
